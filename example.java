139b9142e1c1a27464d96a7ce44442a4c4ed64a4
polarapu <polarapup@gmail.com>
2018-01-03 07:30:32 +0530

    Update example.java

--- example.java
+++ example.java
@@ -5,5 +5,8 @@ public static void main(String[] args){
 System.out.println("Welcome to Devops World");
 }
 
+System.out.println("Welcome to Devops World");
+  
+System.out.println("Welcome to Devops World");
 
 }
